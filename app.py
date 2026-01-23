6ed295798dc9e7e0dd253d466e75213e2e17cd3c
anzarv <anzarvsharif@gmail.com>
2026-01-23 17:40:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -78,9 +78,9 @@ with col3:
         user_input = "L"
 
 # Test variables
-current_day = 1
-current_hour = 14
-current_minute = 35
+# current_day = 1
+# current_hour = 14
+# current_minute = 35
 
 now_mins = (current_hour * 60) + current_minute
 
