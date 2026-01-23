28e9a46e7450ecc477b67444998ef804016bff1b
anzarv <anzarvsharif@gmail.com>
2026-01-23 14:52:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,21 +15,34 @@ st.write("--------")
 st.header(local_time.strftime("%A, %I:%M %p"))
 
 # Get the current minute (0 to 59)
-current_minute = local_time.minute
+# current_minute = local_time.minute
 
 # Get the current hour (0 to 23)
-current_hour = local_time.hour
+# current_hour = local_time.hour
 
 # Get the current day of the week (0 is Monday, 4 is Friday, etc.)
-current_day = local_time.weekday()
+# current_day = local_time.weekday()
 
 # Finding who you're looking for
 
 user_input = st.text_input("Enter 'A' for Anzar, 'E' for Elhussy, 'L' for Labubu: ")
-if user_input == "A":
-   if current_day == 4 and current_hour == 14 and current_minute >= 30 and current_minute < 45:
-           st.write("Anzar is not in class right now.")
 
+current_hour = 13
+current_day = 6
+current_minute = 20
+
+if user_input == "A":
+        if current_day == 6:  # Saturday
+            st.subheader("Anzar's Saturday Schedule")
+
+            if (current_hour == 13) or (current_hour == 14 and current_minute <= 30):
+                st.write("📍 In Class: ECO101 (NAC 605)")
+            elif 10 <= current_hour < 12:
+                st.write("📍 In Class: MAT120 (NAC 502)")
+            elif 14 == current_hour and 30 <= current_minute <= 45:
+                st.write("📍 Free Time: Probably at the Cafeteria")
+            else:
+                st.write("📍 Off-campus or chilling")
 
 
 
