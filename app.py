ed3068804bb7f2ed28c0576d48f599e7742776d8
anzarv <anzarvsharif@gmail.com>
2026-01-23 16:25:56 +0600

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,17 @@ labubu_thurs_sat = [
     (880, 970, "CHE101 (SAC407)")
 ]
 
-elhussy_sun_tue = []
+elhussy_sun_tue = [
+    (780, 870, "ENG103 (NAC409)"),
+    (880, 970, "PSY101 (NAC215")
+]
+
+elhussy_thurs_sat = [
+    (780, 870, "ECO101 (NAC412)"),
+    (870, 980, "Free Period"),
+    (990, 1070, "BUS112 (SAC208)")
+
+]
 
 # Finding who you're looking for
 
@@ -68,7 +78,7 @@ with col3:
         user_input = "L"
 
 current_hour = 3
-current_day = 6
+current_day = 3
 current_minute = 40
 
 now_mins = (current_hour * 60) + current_minute
@@ -120,6 +130,53 @@ if user_input == "A":
             else:
                 # This happens if it's after 5:50 PM
                 st.write("✅ All classes finished for today!")
+    # Thursday (3) or Saturday (5)
+    elif current_day == 3 or 5:
+        st.subheader("Anzar's Thursday / Saturday Tracker")
+
+        # 1. Check if CURRENTLY in class
+        found_now = False
+        for start, end, name in anzar_sun_tue:
+            if start <= now_mins <= end:
+                st.success(f"📍 Currently in: {name}")
+                if not 870 <= now_mins <= 980:
+                    found_now = True
+                    break
+
+                # 2. If NOT in class, find the NEXT one
+        if not found_now:
+            st.write("❌ Not in class right now.")
+
+            next_class = None
+            for start, end, name in anzar_sun_tue:
+                if start > now_mins:
+                    next_class = (start, end, name)
+                    break
+
+            if next_class:
+                start_time = next_class[0]
+                class_name = next_class[2]
+
+                # Countdown math
+                mins_to_go = start_time - now_mins
+                hours_left = mins_to_go // 60
+                rem_mins = mins_to_go % 60
+
+                countdown_text = f"{hours_left}h {rem_mins}m" if hours_left > 0 else f"{rem_mins}m"
+
+                # Start time formatting
+                start_h = start_time // 60
+                start_m = start_time % 60
+                period = "PM" if start_h >= 12 else "AM"
+                display_h = start_h - 12 if start_h > 12 else start_h
+                if display_h == 0: display_h = 12  # Handle 12:00
+
+                st.info(f"⏭️ **Next Class:** {class_name} at {display_h}:{start_m:02d} {period}")
+                st.metric(label="Time til next class:", value=countdown_text)
+            else:
+                # This happens if it's after 5:50 PM
+                st.write("✅ All classes finished for today!")
+
 
 if user_input == "L":
     # Sunday (6) or Tuesday (1)
