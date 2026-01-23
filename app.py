50e275208acd57eb93fd1b9f0a687877218d6e87
anzarv <anzarvsharif@gmail.com>
2026-01-23 14:17:34 +0600

    test

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import datetime
 import streamlit as st
-from streamlit_javascript import st_javascript
-
+import pytz
+from datetime import datetime
 
 # Title screen
 st.title("Where My Homies At?")
-st.header("This application will help you find where your homies are at during their NSU campus schedule.")
-st.subheader("Made by Anzar")
+st.write("This application will help you find where your homies are at during their NSU campus schedule.")
+st.write("Made by Anzar")
 
 st.write("Enter 'A' for Anzar, 'E' for Elhussy, 'B' for Labubu")
 
@@ -18,26 +17,16 @@ user_input = st.text_input("Type something here:")
 if user_input:
    st.write("The Magic Box says:")
    st.header(user_input.upper()) # This makes your input BIG and LOUD
-import streamlit as st
-from streamlit_javascript import st_javascript
-from datetime import datetime
 
-st.title("My Local Time App")
+# 1. Define the GMT+6 timezone
+# In the coding world, GMT+6 is often represented by 'Asia/Dhaka' or 'Etc/GMT-6'
+# (Note: GMT offsets in 'Etc/' are often inverted, so we use a fixed offset)
+target_timezone = pytz.timezone('Asia/Dhaka')
 
-# Ask the phone for the time
-client_time = st_javascript("Date.now()")
+# 2. Get the current time in that timezone
+local_time = datetime.now(target_timezone)
 
-# Check if we have the time yet
-if client_time is not None and client_time > 0:
-    # Convert milliseconds to a datetime object
-    # We add a "timedelta" if the server is stubbornly showing UTC
-    # Since you were 6 hours off, we subtract 6 hours (or add, depending on your zone)
-    raw_time = datetime.fromtimestamp(client_time / 1000)
+# 3. Show it on the screen
+st.write("The current time in GMT+6 is:")
+st.header(local_time.strftime("%I:%M %p"))
 
-    st.write("Your local time is:")
-    st.header(raw_time.strftime("%I:%M %p"))
-
-elif client_time == 0:
-    st.warning("Still waiting for your device to respond...")
-else:
-    st.info("Loading your clock... ⌚")
