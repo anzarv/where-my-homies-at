98bf8aeae8965fe19d63c4d3cdaa3937418805d1
anzarv <anzarvsharif@gmail.com>
2026-01-23 16:10:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,9 @@ if user_input == "L":
         for start, end, name in labubu_sun_tue:
             if start <= now_mins <= end:
                 st.success(f"📍 Currently in: {name}")
-                found_now = True
-                break
+                if not 870 <= now_mins <= 980:
+                    found_now = True
+                    break
 
                 # 2. If NOT in class, find the NEXT one
         if not found_now:
