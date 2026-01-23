6e535602e83f80b0985992aa09c441870ce2e071
anzarv <anzarvsharif@gmail.com>
2026-01-23 16:43:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ current_hour = local_time.hour
 
 # Get the current day of the week (0 is Monday, 4 is Friday, etc.)
 current_day = local_time.weekday()
-st.write(f"Today is {current_day}")
 
 # Everyone's schedules
 
